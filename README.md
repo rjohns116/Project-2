1cd21054cb8c926bdcda18fb23fc81afe211e2e8
rjohns116 <rlynjohnson6@gmail.com>
2026-04-26 20:52:54 +0000

    add all

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+#Candian audience exploration
+
 #dataset: https://open.canada.ca/data/dataset/3793028e-96ff-43cc-9281-bfcd83a114a8?utm_source=copilot.com
---
+++ cleaning.py
@@ -0,0 +1,11 @@
+#Step 1: load in the data to a dataframe from csv
+
+#Step 2: Data cleaning (repeat for tv, radio, audiovisual)
+# Get a look at the dataframe
+# Note what needs to be changed
+# Transform dataframe
+# save as a cleaned file
+
+
+
+
